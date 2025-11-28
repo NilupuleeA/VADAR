1d03e5f7874c5ccc80c9c3318e2a1f9084f52638
nilupuleea-accelr <nilupulee.amarathunga@accelr.site>
2025-11-28 20:28:01 +0530

    modified download.sh

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,7 @@
 
 mkdir -p data
 cd data
+hf download dmarsili/Omni3D-Bench omni3d-bench.zip --repo-type dataset --local-dir .
 unzip omni3d-bench.zip -d omni3d-bench
 pip install gdown
 gdown https://drive.google.com/uc?id=1gWxr19YjQQAvgIUQSsw2Kr4w-q-F0pgx
