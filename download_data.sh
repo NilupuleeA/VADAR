600ceda6f94cea8ba7d5295f9242912d89b02128
nilupuleea-accelr <nilupulee.amarathunga@accelr.site>
2025-12-06 04:09:46 +0530

    Added scenes.json

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-mkdir -p data
 cd data
 hf download dmarsili/Omni3D-Bench omni3d-bench.zip --repo-type dataset --local-dir .
 unzip omni3d-bench.zip -d omni3d-bench
 pip install gdown
 gdown https://drive.google.com/uc?id=1gWxr19YjQQAvgIUQSsw2Kr4w-q-F0pgx
-unzip clevr_subset.zip
+unzip clevr_subset.zip -d clevr_subset
