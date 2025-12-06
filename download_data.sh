e17a36d6b67ea48c5ab8254ee0d3786500a7f5f2
nilupuleea-accelr <nilupulee.amarathunga@accelr.site>
2025-12-06 13:43:22 +0530

    Changed data file

--- download_data.sh
+++ download_data.sh
@@ -4,4 +4,8 @@ hf download dmarsili/Omni3D-Bench omni3d-bench.zip --repo-type dataset --local-d
 unzip omni3d-bench.zip -d omni3d-bench
 pip install gdown
 gdown https://drive.google.com/uc?id=1gWxr19YjQQAvgIUQSsw2Kr4w-q-F0pgx
-unzip clevr_subset.zip -d clevr_subset
+
+mkdir -p clevr_subset_tmp
+unzip -n clevr_subset.zip -d clevr_subset_tmp
+mv clevr_subset_tmp/*/* clevr_subset/
+rm -r clevr_subset_tmp
